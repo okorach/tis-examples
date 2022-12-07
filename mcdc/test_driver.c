e78da0f7e863cc3d62f863d6f4e99345f44b570f
okorach <olivier.korach@gmail.com>
2022-12-07 14:07:14 -0800

    Break MCDC example into pieces

--- mcdc/test_driver.c
+++ mcdc/test_driver.c
@@ -21,19 +21,36 @@ int test_generalized_mcdc1(void)
 }
 #endif
 
-int main(void)
+void test_mcdc_base()
 {
     mcdc1(0, 0, 0);
     mcdc1(0, 0, 1);
     mcdc1(1, 1, 0);
     mcdc1(1, 1, 1);
+}
 
+void test_mcdc_complement_1()
+{
     mcdc1(0, 1, 0);
-    // mcdc1(1, 0, 0);
+}
 
-    // mcdc1(0, 1, 1);
-    // mcdc1(1, 0, 1);
-    
+void test_mcdc_complement_2()
+{
+    mcdc1(1, 0, 0);
+}
+
+void test_mcdc_complement_3()
+{
+    mcdc1(0, 1, 1);
+    mcdc1(1, 0, 1);
+}
+
+int main(void)
+{
+    test_mcdc_base();
+    test_mcdc_complement_1();
+    test_mcdc_complement_2();
+    test_mcdc_complement_3();
 #if defined LEVEL2
     test_generalized_mcdc1();
 #endif
